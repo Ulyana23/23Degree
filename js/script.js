adda590ead2e4a160e34ada5c432b2072ebeaddc
Ulyana23 <43664758+Ulyana23@users.noreply.github.com>
2020-11-05 17:18:36 +0300

    some js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,56 @@
 $(document).ready(function() {
         $("#radios").radiosToSlider();
+        //--slider
         $(".range").attr('max', $(".slider__item").width() * 21 - $(".slider__nav").width() + 90);
+
+        let timerSwipe = setInterval(() => $(".oval").toggleClass("active"), 400);
+        setTimeout(() => { clearInterval(timerSwipe); }, 1600);
 });
 
 
-//сделать реакцию на изменение размера окна
+//###--MENU--###
+$(".intro__menu").click(function () {
+        $(".menu").addClass("show");
+});
+
+$(".menu__close").click(function () {
+        $(".menu").removeClass("show");
+});
+
+
+//###--INTRO_SWIPE--###
+$(".intro__swipe").click(function () {
+        var blockOffset = $(".services").offset().top;
+        $("html, body").animate({
+                scrollTop: blockOffset
+        }, 500);
+});
+
+$(window).scroll(function () {
+        //alert($(window).scrollTop() + $(window).height());
+        if($(window).scrollTop() == 0) {
+                $(".point").removeClass("active");
+        }
+        else $(".point").addClass("active");
+});
+
+//###--SERVICES--##
+$(".services button").click(function () {
+        $("html, body").animate({
+                scrollTop: 0
+        }, 500);
+});
+
+
+//###--SLIDER--###
 $(".range").on('input', function() {
         $(".slider__nav").scrollLeft($(".range").val());
 });
 
 $(".slider__nav").scroll(function () {
         $(".range").val($(".slider__nav").scrollLeft());
+});
+
+$(window).resize(function() {
+        $(".range").attr('max', $(".slider__item").width() * 21 - $(".slider__nav").width() + 90);
 });
